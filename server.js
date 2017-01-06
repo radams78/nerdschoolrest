a95bc28d160c9fd1bf790729451a348a16811f7b
Robin Adams <robin.adams.78@gmail.com>
2017-01-06 11:27:21 +0100

    Add logging

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const app = express()
-app.get('/', (request, response) => response.send('Hello World'));
+const morgan = require('morgan')
+
 const port = 3000;
+
+const app = express()
+app.use(morgan('dev'))
+app.get('/', (request, response) => response.send('Hello World'));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
